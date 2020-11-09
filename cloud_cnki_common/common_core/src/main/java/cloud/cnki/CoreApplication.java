4f46d200a4dce90cc2bb66c7a8a21dc89053b9cc
Durjx <28987930@qq.com>
2020-11-09 23:50:45 +0800

    core

---
+++ cloud_cnki_common/common_core/src/main/java/cloud/cnki/CoreApplication.java
@@ -0,0 +1,13 @@
+package cloud.cnki;
+
+/**
+ * Hello world!
+ *
+ */
+public class CoreApplication
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ cloud_cnki_common/common_core/src/main/java/cloud/cnki/core/config/thread/ThreadPoolConfig.java
@@ -0,0 +1,40 @@
+package cloud.cnki.core.config.thread;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * ThreadPoolConfig
+ * 线程池
+ * @author durjx
+ * @date 2020-11-09
+ */
+@Configuration
+public class ThreadPoolConfig {
+
+    // 核心线程池大小
+    private int corePoolSize = 50;
+
+    // 最大可创建的线程数
+    private int maxPoolSize = 200;
+
+    // 队列最大长度
+    private int queueCapacity = 1000;
+
+    // 线程池维护线程所允许的空闲时间
+    private int keepAliveSeconds = 300;
+
+    public ThreadPoolTaskExecutor threadPoolTashExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setCorePoolSize(corePoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setKeepAliveSeconds(keepAliveSeconds);
+        // 线程池对拒绝任务(无线程可用)的处理策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        return  executor;
+    }
+}
